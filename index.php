796554a514d5df0469eb6995d352a5d7d4266fe5
SergioGonzalezB11 <sagonzalezbarajas@gmail.com>
2023-05-26 09:16:58 -0500

    add

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Television 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo',
+        'precio' => 200,
+        'disponible' => false
+    ]
+    
+    ];
+?>
+<?php
+    require_once('producto.php');
+    $productos = array (new Producto("Tablet",200,5,true),
+                        new Producto("Television 24",300,4,true),
+                        new Producto("Monitor Curvo",200,3,false));
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <main>
+        <pre>
+        <?php
+            var_dump($productos);
+            $json = json_encode($productos,JSON_UNESCAPED_UNICODE);
+            ?>
+            <br>
+            <?php
+                var_dump($json);
+            ?>
+        <pre>
+    </main> 
+    <div>
+        <?php  foreach($productos as $producto) { ?>
+                <li>
+                    <p> Producto: <?php echo $producto['nombre'];?></p>
+                    <p> Precio: <?php echo $producto['precio'];?></p>
+                    <p> <?php echo ($producto['disponible']) ? 'Disponible':'No disponible';?> </p>
+                </li>
+        <?php } ?>
+    </div> 
+    <div>
+        <table>
+            <thead>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Disponibilidad</th>
+            </thead>
+            <tbody>
+                <?php
+                foreach($productos as $producto):?>
+                    <tr>
+                        <td> <?php echo $producto['nombre'];?></td>
+                        <td> <?php echo $producto['precio'];?></td>
+                        <td> <?php echo ($producto['disponible']) ? 'Disponible':'No disponible';?> </td>
+                    </tr>
+                    <?php endforeach?>
+            </tbody>
+        </table>
+    </div>
+    <div>
+        <?php foreach($productos as $producto):?>
+            <p><?php echo $producto-> getNombre();?></p>
+            <?php endforeach?>
+    </div>
+</body>
+</html>    
---
+++ producto.php
@@ -0,0 +1,42 @@
+<?php
+    class Producto {
+        private $nombre;
+        private $precio;
+        private $cantidad;
+        private $disponible;
+
+        public function __construct($nombre, $precio, $cantidad, $disponible){
+            $this -> nombre = $nombre;
+            $this -> precio = $precio;
+            $this -> cantidad = $cantidad;
+            $this -> disponible = $disponible;
+
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+        public function getPrecio(){
+            return $this->precio;
+        }
+        public function getCantidad(){
+            return $this->cantidad;
+        }
+        public function getDisponible(){
+            return $this->disponible;
+        }
+        public function setNombre($nombre){
+            $this -> nombre = $nombre;
+        }
+        public function setPrecio($precio){
+            $this -> precio = $precio;
+        }
+        public function setCantidad($cantidad){
+            $this -> cantidad = $cantidad;
+        }
+        public function setDisponible($disponible){
+            $this -> disponible = $disponible;
+        }
+    }
+?>
+
